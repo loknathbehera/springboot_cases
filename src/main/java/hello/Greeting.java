f1e128dc6ef5eb2f7c6c78f485c1b8f4f0390694
Lokanath Behera <lb.dvlpr@gmail.com>
2017-04-15 21:51:52 +0530

    DOne

--- src/main/java/hello/Greeting.java
+++ src/main/java/hello/Greeting.java
@@ -1,17 +1,17 @@
 package hello;
 
-public class Greeting {
+import org.springframework.hateoas.ResourceSupport;
 
-	private final long id;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Greeting extends ResourceSupport {
+
 	private final String content;
 
-	public Greeting(long id, String content) {
-		this.id = id;
+	@JsonCreator
+	public Greeting(@JsonProperty("content") String content) {
 		this.content = content;
-	}
-
-	public long getId() {
-		return id;
 	}
 
 	public String getContent() {
--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,19 +1,26 @@
 package hello;
 
-import java.util.concurrent.atomic.AtomicLong;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
 
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
+	private static final String TEMPLATE = "Hello, %s!";
 
-    @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
-    }
+	@RequestMapping("/greeting")
+	public HttpEntity<Greeting> greeting(
+			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+
+		Greeting greeting = new Greeting(String.format(TEMPLATE, name));
+		greeting.add(linkTo(methodOn(GreetingController.class).greeting(name)).withSelfRel());
+
+		return new ResponseEntity<Greeting>(greeting, HttpStatus.OK);
+	}
 }
